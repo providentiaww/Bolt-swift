cedf66e4d22de06a127c91221bbe95dd5cf9d9fd
S. Ryan Quick <ryan@phaedo.com>
2019-06-20 00:56:58 +0200

    updated for version 5 of swift

--- Package.swift
+++ Package.swift
@@ -3,16 +3,13 @@ import PackageDescription
 
 let package = Package(
     name: "Bolt",
-	products: [
-		.library(name: "Bolt", targets: ["Bolt"]),
-	],
+	products: [ .library(name: "Bolt", targets: ["Bolt"])],
 	dependencies: [
-	    .package(url: "https://github.com/Neo4j-Swift/PackStream-swift.git",
-	                 from: "1.0.0"),
-		.package(url: "https://github.com/IBM-Swift/BlueSSLService.git", from: "1.0.44"),
+	    .package(url: "https://github.com/Neo4j-Swift/PackStream-swift.git", from: "1.0.0"),
+      .package(url: "https://github.com/IBM-Swift/BlueSSLService.git", from: "1.0.44"),
 	    ],
 	targets: [
-		.target( name: "Bolt" , 
+		.target( name: "Bolt" ,
 			dependencies: ["PackStream", "SSLService"],
 			path: "" )
 		]
