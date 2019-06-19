ee6ada1c4b8ba3bc296035624e4b288255e98bda
Ryan <ryan.quick@providentiaworldwide.com>
2019-06-19 14:35:55 +0000

    uprev to support swift 5 and newer versions of the IBM libraries

--- Package.swift
+++ Package.swift
@@ -1,10 +1,19 @@
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
     name: "Bolt",
+	products: [
+		.library(name: "Bolt", targets: ["Bolt"]),
+	],
 	dependencies: [
-	    .Package(url: "https://github.com/Neo4j-Swift/PackStream-swift.git",
-	                 majorVersion: 1),
-		.Package(url: "https://github.com/IBM-Swift/BlueSSLService.git", majorVersion: 0),
-	    ]
+	    .package(url: "https://github.com/Neo4j-Swift/PackStream-swift.git",
+	                 from: "1.0.0"),
+		.package(url: "https://github.com/IBM-Swift/BlueSSLService.git", from: "1.0.44"),
+	    ],
+	targets: [
+		.target( name: "Bolt" , 
+			dependencies: ["PackStream", "SSLService"],
+			path: "" )
+		]
 )
